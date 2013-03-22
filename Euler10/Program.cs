4e0408fb8f53a23ad0958e0ef19a09302a10af30
Max Maton <thexa4@gmail.com>
2013-03-22 22:48:52 +0100

    Example Euler10 solution

---
+++ Euler10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 2000000;
+            var s = new Sieve(size);
+
+            long count = 0;
+
+            foreach (int prime in s.GetPrimes())
+                count += prime;
+
+            Console.Write(count);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Euler10/Sieve.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler10
+{
+    class Sieve
+    {
+        public int Size { get; protected set; }
+        protected bool[] Primes { get; set; }
+
+        public Sieve(int maxprime)
+        {
+            Size = maxprime;
+            Primes = new bool[maxprime + 1];
+            Primes[1] = true; //1 is not a prime
+        }
+
+        public IEnumerable<int> GetPrimes()
+        {
+            int curreturn = 1;
+            int maxsieve = (int)Math.Sqrt(Size) + 1;
+
+            // Mark all multiples of 2 as nonprime
+            for (int j = 2 * 2; j <= Size; j += 2)
+                Primes[j] = true;
+            yield return 2;
+
+            // We only need to do this for the sqrt of the size
+            for (int i = 3; i <= maxsieve; i += 2)
+            {
+                // If this number is prime, all numbers that are a multiple of this one
+                // are not prime
+                if (Primes[i] == false)
+                    for (int j = 2 * i; j <= Size; j += i)
+                        Primes[j] = true;
+
+                // Return all numbers we know to be prime for certain
+                int maxreturn = i * i;
+                for (; curreturn <= maxreturn && curreturn <= Size; curreturn++)
+                    if (Primes[curreturn] == false)
+                        yield return curreturn;
+            }
+        }
+    }
+}
